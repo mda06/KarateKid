e2ac10d1da0577a87bf96ea3f1a914c33f5e274e
mda06 <michaelvm171@msn.com>
2016-02-29 12:34:20 +0100

    Created Entity class

---
+++ KarateKid/Entity.h
@@ -0,0 +1,16 @@
+//
+//  Entity.h
+//  KarateKid
+//
+//  Created by Michaël Van Meerbeek on 29/02/16.
+//  Copyright (c) 2016 Michaël Van Meerbeek. All rights reserved.
+//
+
+#ifndef __KarateKid__Entity__
+#define __KarateKid__Entity__
+
+class Entity
+{
+};
+
+#endif /* defined(__KarateKid__Entity__) */
